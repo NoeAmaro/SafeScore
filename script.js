4d3f61f674e696d8117be253b7c7e2e34c062f30
Noe Jain <161240758+NoeAmaro@users.noreply.github.com>
2024-11-24 19:05:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('login-btn').addEventListener('click', () => {
+    // Simula un inicio de sesión exitoso
+    window.location.href = 'results.html';
+  });
+  
